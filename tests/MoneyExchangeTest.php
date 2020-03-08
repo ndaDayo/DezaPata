d3d376fa2038a6840a9ef77f88e320aa0c595a31
ndaDayo <hayato711@gmail.com>
2020-03-09 00:17:57 +0900

    CurrencyConversionのテストを追加[FAIL]

--- tests/MoneyExchangeTest.php
+++ tests/MoneyExchangeTest.php
@@ -14,29 +14,38 @@ class MoneyExchangeTest extends PHPUnit_Framework_TestCase
 
     private $yen;
 
+    /** @var float */
     private $doll_rate;
 
-    /**
-     * Sets up the fixture, for example, opens a network connection.
-     * This method is called before a test is executed.
-     */
+    /** @var float */
+    private $eur_rate;
+
+    /** @var float */
+    private $pond_rate;
+
+
     protected function setUp()
     {
         $this->yen = 100;
         $this->moneyExchange = new MoneyExchange($this->yen);
+
         $this->doll_rate = 1.1048;
+        $this->eur_rate = 1.2978;
+        $this->pond_rate = 1.4681;
     }
 
     /**
      * @test
      */
-    public function 円をドルに変換する()
+    public function testCurrencyConversion()
     {
-        $country = "US";
-        $expected = $this->doll_rate * $this->yen;
+        $expected_us = $this->doll_rate * $this->yen;
+        $expected_eur = $this->eur_rate * $this->yen;
+        $expected_pond = $this->pond_rate * $this->yen;
 
-        /** @var TYPE_NAME $country */
-        $this->assertEquals($expected, $this->moneyExchange->currencyConversion($country));
+        $this->assertEquals($expected_us, $this->moneyExchange->currencyConversion("US"));
+        $this->assertEquals($expected_eur, $this->moneyExchange->currencyConversion("EUR"));
+        $this->assertEquals($expected_pond, $this->moneyExchange->currencyConversion("POND"));
     }
 
 }
