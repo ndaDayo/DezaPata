8cad5d0173109eb1278f4fd9095cf3c31a08489b
ndaDayo <hayato711@gmail.com>
2020-03-09 23:18:53 +0900

    DollarExchangeのテストと実装追加[PASS]

--- App/Exchange/DollarExchange.php
+++ App/Exchange/DollarExchange.php
@@ -1,10 +1,22 @@
 <?php
 
-
 namespace App\Exchange;
 
+use App\ExchangeInterface\ExchangeInterface;
 
-class DollarExchange
+class DollarExchange implements ExchangeInterface
 {
+    private $yen;
 
+    private $rate = 1.1048;
+
+    public function __construct($yen)
+    {
+        $this->yen = $yen;
+    }
+
+    public function currencyConversion()
+    {
+        return $this->yen * $this->rate;
+    }
 }
